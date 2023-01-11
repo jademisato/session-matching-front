25d9a19847c9e9a15919256b3ec160607a7af6bc
jademisato <87796782+jademisato@users.noreply.github.com>
2023-01-11 15:49:30 +0900

    Change root page to Server Components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
-export default function Test() {
+export default function Page() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h2>Hello  world</h2>
-
+        <ul>
+          <li>
+            < Link href="/test">Testpage1</Link>
+          </li>
+          <li>
+            < Link href="/products/dog">Dog1</Link>
+          </li>
+          <li>
+            < Link href="/products/cat">Cat1</Link>
+          </li>
+        </ul>
+        {/* ↓ Leave it because the link will not be displayed unless it is written in duplicate. */}
         <ul>
           <li>
             < Link href="/test">Testpage</Link>
